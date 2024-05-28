82fb2eecc4ee4752713e05394e96a1daf204bee6
sirsegv <squidinkrf@gmail.com>
2024-05-28 21:57:08 +0930

    Update syntax to modern V

--- nanoid.v
+++ nanoid.v
@@ -3,17 +3,13 @@ module nanoid
 import math
 import crypto.rand
 
-const (
-	// default_alphabet is the alphabet used for ID characters by default.
-	default_alphabet = '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.runes()
-	// default length for ID
-	default_size     = 21
-)
+// default_alphabet is the alphabet used for ID characters by default.
+const default_alphabet = '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.runes()
 
 // get_mask generates bit mask used to obtain bits from the random bytes that are used to get index of random character
 // from the alphabet. Example: if the alphabet has 6 = (110)_2 characters it is sufficient to use mask 7 = (111)_2
 fn get_mask(alphabet_size int) int {
-	for i := 1; i <= 8; i++ {
+	for i in 1..9 {
 		mask := (2 << u64(i)) - 1
 		if mask >= alphabet_size - 1 {
 			return mask
@@ -23,7 +19,7 @@ fn get_mask(alphabet_size int) int {
 }
 
 // generate is a low-level function to change alphabet and ID size.
-pub fn generate(alphabet string, size int) ?string {
+pub fn generate(alphabet string, size int) !string {
 	chars := alphabet.runes()
 
 	if alphabet.len == 0 || alphabet.len > 255 {
@@ -40,10 +36,12 @@ pub fn generate(alphabet string, size int) ?string {
 	step := int(math.ceil(ceil_arg))
 
 	mut id := []rune{len: size}
-	// bytes := []byte{len: step}
+	
 	bytes := rand.read(step) or { return error(err.msg()) }
-	for j := 0; true; {
-		for i := 0; i < step; i++ {
+	
+	mut j := 0
+	for true {
+		for i in 0..step {
 			curr_byte := bytes[i] & u8(mask)
 			if curr_byte < u8(chars.len) {
 				id[j] = chars[curr_byte]
@@ -55,34 +53,31 @@ pub fn generate(alphabet string, size int) ?string {
 		}
 	}
 
-	return error('could not generated')
+	return error('could not generate')
 }
 
 // must_generate is the same as generate but panics on error.
 pub fn must_generate(alphabet string, size int) string {
-	id := generate(alphabet, size) or { panic(err.msg()) }
-	return id
+	return generate(alphabet, size) or { panic(err.msg()) }
+}
+
+@[params]
+pub struct NanoIDParams {
+	size int = 21
 }
 
 // new generates secure URL-friendly unique ID.
 // Accepts optional parameter - length of the ID to be generated (21 by default).
-pub fn new(l ...int) ?string {
-	mut size := int(0)
-	if l.len == 0 {
-		size = default_size
-	} else if l.len == 1 {
-		size = l[0]
-		if size <= 0 {
-			return error('size must be positive integer')
-		}
-	} else {
-		return error('unexpected parameter')
+pub fn new(p NanoIDParams) !string {
+	mut size := p.size
+	if size <= 0 {
+		return error('size must be positive integer')
 	}
 
-	bytes := rand.read(size) or { return error(err.msg()) }
+	bytes := rand.read(size)!
 
 	mut id := []rune{len: size}
-	for i := 0; i < size; i++ {
+	for i in 0..size {
 		id[i] = default_alphabet[bytes[i] & 63]
 	}
 
@@ -90,7 +85,6 @@ pub fn new(l ...int) ?string {
 }
 
 // must is the same as new but panics on error.
-pub fn must(l ...int) string {
-	id := new(...l) or { panic(err.msg()) }
-	return id
+pub fn must(p NanoIDParams) string {
+	return new(p) or { panic(err.msg()) }
 }
